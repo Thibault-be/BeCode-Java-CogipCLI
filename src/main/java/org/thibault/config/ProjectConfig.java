68133bce86e35e8de14f33b3228a1cc0321196e2
Thibault-be <thibault.dhooghe@gmail.com>
2024-03-18 10:41:40 +0100

    Clean up ProjectConfig class.

--- src/main/java/org/thibault/config/ProjectConfig.java
+++ src/main/java/org/thibault/config/ProjectConfig.java
@@ -1,32 +1,11 @@
 package org.thibault.config;
 
-import feign.RequestInterceptor;
-import feign.auth.BasicAuthRequestInterceptor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.thibault.model.AuthResponseDTO;
 
 @Configuration
-@ComponentScan("org.thibault")
 @EnableFeignClients(basePackages = "org.thibault.proxy")
 public class ProjectConfig {
-  
-//  @Value("${cogip.username}")
-//  private String username;
-//
-//  @Value("${cogip.password}")
-//  private String password;
-//
-//  @Bean
-//  public BasicAuthRequestInterceptor basicAuthRequestInterceptor(){
-//    return new BasicAuthRequestInterceptor(username, password);
-//  }
-  
 
 }
 
