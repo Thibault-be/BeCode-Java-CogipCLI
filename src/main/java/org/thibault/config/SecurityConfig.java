c0cb3c492b31ab94222c230d44694519b21285ed
Thibault-be <thibault.dhooghe@gmail.com>
2024-03-18 10:38:37 +0100

    Remove unnecessary SecurityConfig class.

--- src/main/java/org/thibault/config/SecurityConfig.java
+++
@@ -1,8 +0,0 @@
-package org.thibault.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.config.EnableWebFlux;
-
-@Configuration
-public class SecurityConfig {
-}
