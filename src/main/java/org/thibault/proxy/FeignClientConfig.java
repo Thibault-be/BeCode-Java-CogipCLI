04ff4359fcb863347d7ae6c0c5a41c895d85aeb9
Thibault-be <thibault.dhooghe@gmail.com>
2024-03-18 10:13:55 +0100

    Remove redundant imports in FeignClientConfig.

--- src/main/java/org/thibault/proxy/FeignClientConfig.java
+++ src/main/java/org/thibault/proxy/FeignClientConfig.java
@@ -1,11 +1,8 @@
 package org.thibault.proxy;
 
 import feign.RequestInterceptor;
-import jakarta.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.thibault.controllers.AuthController;
 
 @Configuration
 public class FeignClientConfig {
