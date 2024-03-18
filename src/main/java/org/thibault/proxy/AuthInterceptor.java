89a910232ad7c95d772cd9b2aa8a199a9f439d03
Thibault-be <thibault.dhooghe@gmail.com>
2024-03-18 10:11:40 +0100

    Remove redundant constructors and imports in AuthInterceptor

--- src/main/java/org/thibault/proxy/AuthInterceptor.java
+++ src/main/java/org/thibault/proxy/AuthInterceptor.java
@@ -2,9 +2,7 @@ package org.thibault.proxy;
 
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
-import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.thibault.controllers.AuthController;
 import org.thibault.services.AuthService;
 
 public class AuthInterceptor implements RequestInterceptor {
@@ -12,19 +10,9 @@ public class AuthInterceptor implements RequestInterceptor {
   @Autowired
   private AuthService authService;
   
-  public AuthInterceptor(){}
-  
-  public AuthInterceptor(AuthService authService) {
-    this.authService = authService;
-  }
-  
   @Override
   public void apply(RequestTemplate requestTemplate) {
     String token = this.authService.getJwToken();
     requestTemplate.header("Authorization", "Bearer " + token);
   }
-  
-  
-
-  
 }
