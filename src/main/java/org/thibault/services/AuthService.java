2345ecf410bc1c828c022b38e008ba7c4e6bd12c
Thibault-be <thibault.dhooghe@gmail.com>
2024-03-18 09:51:22 +0100

    Add instance variable and getters and setters to AuthService

--- src/main/java/org/thibault/services/AuthService.java
+++ src/main/java/org/thibault/services/AuthService.java
@@ -1,4 +1,18 @@
 package org.thibault.services;
 
 public class AuthService {
+  
+  String jwToken;
+  
+  public AuthService (String jwToken){
+    this.jwToken = jwToken;
+  }
+  
+  public String getJwToken() {
+    return jwToken;
+  }
+  
+  public void setJwToken(String jwToken) {
+    this.jwToken = jwToken;
+  }
 }
