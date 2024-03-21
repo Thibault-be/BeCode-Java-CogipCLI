abee4ffbc4dc86b8a183aa6404067b863058b7d5
Thibault-be <thibault.dhooghe@gmail.com>
2024-03-21 10:53:17 +0100

    Contacts now printable as Json.

--- src/main/java/org/thibault/commands/ContactCommand.java
+++ src/main/java/org/thibault/commands/ContactCommand.java
@@ -1,10 +1,14 @@
 package org.thibault.commands;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import org.springframework.shell.command.annotation.Command;
 import org.springframework.shell.command.annotation.Option;
 import org.thibault.controllers.ContactController;
 import org.thibault.model.CompanyDTO;
 import org.thibault.model.ContactDTO;
+import org.thibault.model.joindto.JoinCompanyDTO;
+import org.thibault.model.joindto.JoinContactDTO;
 
 import java.util.List;
 
@@ -25,14 +29,15 @@ import java.util.List;
                                            @Option(longNames = {"lastname", "last"}, shortNames = 'l') String lastname,
                                            @Option(longNames = "phone", shortNames = 'p') String phone,
                                            @Option(longNames = "email", shortNames = 'e') String email,
-                                           @Option(longNames = {"companyId", "company"}, shortNames = 'c') Integer companyId) {
+                                           @Option(longNames = {"companyId", "company"}, shortNames = 'c') Integer companyId,
+                                           @Option(longNames = "json", shortNames = 'j') String json) {
       
       switch (crud) {
         case ("get"):
           if (id == null && firstname == null && lastname == null && phone == null && companyId == null){
-            getAllContacts();
+            getAllContacts(json);
           } else {
-            getContactsByFilters(id, firstname, lastname, phone, companyId);
+            getContactsByFilters(id, firstname, lastname, phone, companyId, json);
           }
           break;
         case ("post"):
@@ -48,14 +53,18 @@ import java.util.List;
       return null;
     }
     
-    private void getAllContacts(){
-      this.contactController.getAllContacts()
-              .forEach(contact -> System.out.println(contact));
+    private void getAllContacts(String json){
+      List<JoinContactDTO> contacts = this.contactController.getAllContacts();
+      printContactsList(contacts, json);
+      
+      //this.contactController.getAllContacts()              .forEach(contact -> System.out.println(contact));
     }
     
-    private void getContactsByFilters(Integer id, String firstname, String lastname, String phone, Integer companyId){
-      this.contactController.getContactsByFilters(id, firstname, lastname, phone, companyId)
-              .forEach(System.out::println);
+    private void getContactsByFilters(Integer id, String firstname, String lastname, String phone, Integer companyId, String json){
+      List<JoinContactDTO> filteredContacts = this.contactController.getContactsByFilters(id, firstname, lastname, phone, companyId);
+      printContactsList(filteredContacts, json);
+      
+      //this.contactController.getContactsByFilters(id, firstname, lastname, phone, companyId)              .forEach(System.out::println);
     }
     
     private void addContact(String firstname, String lastname, String phone, String email, Integer companyId){
@@ -71,6 +80,19 @@ import java.util.List;
     private void deleteContact(int id){
       System.out.println(this.contactController.deleteContact(id));
     }
+    
+    private void printContactsList(List<JoinContactDTO> contacts, String json){
+      if (json != null && (json.equals("j") || json.equals("json"))){
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        System.out.println(gson.toJson(contacts));
+      } else {
+        for (JoinContactDTO contact : contacts) {
+          System.out.println(contact.toString());
+        }
+      }
+    }
+    
+    
   }
   
 
