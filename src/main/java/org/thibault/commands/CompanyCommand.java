76ee11a1e5dba4562a2559dfccb2f424316d88f8
Thibault-be <thibault.dhooghe@gmail.com>
2024-03-21 10:38:13 +0100

    Companies now printable as Json.

--- src/main/java/org/thibault/commands/CompanyCommand.java
+++ src/main/java/org/thibault/commands/CompanyCommand.java
@@ -1,5 +1,7 @@
 package org.thibault.commands;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import feign.FeignException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.shell.command.annotation.Command;
@@ -8,6 +10,9 @@ import org.thibault.controllers.CompanyController;
 import org.thibault.enums.CompanyType;
 import org.thibault.enums.converters.EnumConverter;
 import org.thibault.model.CompanyDTO;
+import org.thibault.model.UserDTO;
+import org.thibault.model.joindto.JoinCompanyDTO;
+
 import java.util.List;
 
 @Command (group= "Company commands", description = "Commands related to company data.")
@@ -22,22 +27,24 @@ public class CompanyCommand {
                                          @Option(longNames = "name", shortNames = 'n') String name,
                                          @Option(longNames = "country", shortNames = 'c') String country,
                                          @Option(longNames = "vat", shortNames = 'v') String vat,
-                                         @Option(longNames = "type", shortNames = 't') String type) {
+                                         @Option(longNames = "type", shortNames = 't') String type,
+                                         @Option(longNames = "json", shortNames = 'j') String json
+  ) {
     CompanyType companyType = new EnumConverter().convertStringToCompanyType(type);
     
     switch (crud) {
       case ("get"):
         if (id == null && name == null && country== null && vat == null && type == null){
-          getAllCompanies();
+          getAllCompanies(json);
         }else{
-          getCompaniesByFilters(id, name, country, vat, companyType);
+          getCompaniesByFilters(id, name, country, vat, companyType, json);
         }
         break;
       case("post"):
         addCompany(name, country, vat, companyType);
         break;
       case("put"):
-        updateCompany(id, name, country, vat, companyType);
+        updateCompany(id, name, country, vat, companyType, json);
         break;
       case("delete"):
         deleteCompany(id);
@@ -46,13 +53,18 @@ public class CompanyCommand {
     return null;
   }
   
-  private void getAllCompanies(){
-    this.companyController.getAllCompanies().forEach(joinCompanyDTO -> System.out.println(joinCompanyDTO));
+  private void getAllCompanies(String json){
+    List<JoinCompanyDTO> companies = this.companyController.getAllCompanies();
+    printCompanyList(companies, json);
+    
+    //this.companyController.getAllCompanies().forEach(joinCompanyDTO -> System.out.println(joinCompanyDTO));
   }
   
-  private void getCompaniesByFilters(Integer id, String name, String country, String vat, CompanyType type){
-    this.companyController.searchCompaniesByFilters(id, name, country, vat, type)
-            .forEach(joinCompanyDTO -> System.out.println(joinCompanyDTO));
+  private void getCompaniesByFilters(Integer id, String name, String country, String vat, CompanyType type, String json){
+    List<JoinCompanyDTO> filteredCompanies =  this.companyController.searchCompaniesByFilters(id, name, country, vat, type);
+    printCompanyList(filteredCompanies, json);
+    
+    //this.companyController.searchCompaniesByFilters(id, name, country, vat, type).forEach(joinCompanyDTO -> System.out.println(joinCompanyDTO));
   }
   
   private void addCompany(String name, String country, String vat, CompanyType type){
@@ -61,15 +73,15 @@ public class CompanyCommand {
     System.out.println("Company " + companyDTO.getName() + " was successfully added.");
   }
   
-  private void updateCompany(int id, String name, String country, String vat, CompanyType type){
+  private void updateCompany(int id, String name, String country, String vat, CompanyType type, String json){
     System.out.println("Old information:");
-    getCompaniesByFilters(id, null,null, null,null);
+    getCompaniesByFilters(id, null,null, null,null, json);
     
     CompanyDTO companyToUpdate = new CompanyDTO(name, country, vat, type);
     this.companyController.updateCompany(id, companyToUpdate);
     
     System.out.println("\nNew information:");
-    getCompaniesByFilters(id, null,null, null,null);
+    getCompaniesByFilters(id, null,null, null,null, json);
   }
   
   private void deleteCompany(int id){
@@ -77,6 +89,17 @@ public class CompanyCommand {
       System.out.println(this.companyController.deleteCompany(id));
     } catch (FeignException.NotFound ex){
       System.out.println(ex.contentUTF8());
+    }
+  }
+  
+  private void printCompanyList(List<JoinCompanyDTO> companies, String json){
+    if (json != null && (json.equals("j") || json.equals("json"))){
+      Gson gson = new GsonBuilder().setPrettyPrinting().create();
+      System.out.println(gson.toJson(companies));
+    } else {
+      for (JoinCompanyDTO user : companies) {
+        System.out.println(user.toString());
+      }
     }
   }
 }
